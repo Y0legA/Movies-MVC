74dbcd3eb148a8d8b443d1c354055fe68b4b7747
Oleg <myaddub@gmail.com>
2022-10-24 11:19:20 +0300

    init()

---
+++ Movies-MVC/Movies-MVC/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Movies-MVC
+//
+//  Created by Oleg_Yakovlev on 24.10.22.
+//
+
+import UIKit
+///
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
